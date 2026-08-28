8122a5ca5334ea0aad2405b17920c6e129bc117d
Ronnie Dutta <61982285+MetRonnie@users.noreply.github.com>
2026-08-28 15:12:10 +0100

    Delete test

--- test_main.py
+++ test_main.py
@@ -1,14 +1,6 @@
 import pytest
 
-from main import greater_than_five, less_than_ten, is_zero
-
-
-@pytest.mark.parametrize('input, expected', [
-    (6, True),
-    (5, False),
-])
-def test_greater_than_five(input, expected):
-    assert greater_than_five(input) == expected
+from main import less_than_ten, is_zero
 
 
 @pytest.mark.parametrize('input, expected', [
