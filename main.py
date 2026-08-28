8f6f0fccdbc5be52f7e73fc97fa51faabc4fe4f4
Ronnie Dutta <61982285+MetRonnie@users.noreply.github.com>
2026-08-28 14:57:17 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+def greater_than_five(x):
+    if x > 5:
+        return True
+    else:
+        return False
+
+# Lorem ipsum dolor sit amet
+# consectetur adipiscing elit
+
+# adipiscing elit lorem whatever
+
+
+def less_than_ten(x):
+    if x < 10:
+        return True
+    else:
+        return False
---
+++ test_main.py
@@ -0,0 +1,19 @@
+import pytest
+
+from main import greater_than_five, less_than_ten
+
+
+@pytest.mark.parametrize('input, expected', [
+    (6, True),
+    (5, False),
+])
+def test_greater_than_five(input, expected):
+    assert greater_than_five(input) == expected
+
+
+@pytest.mark.parametrize('input, expected', [
+    (9, True),
+    (10, False),
+])
+def test_less_than_ten(input, expected):
+    assert less_than_ten(input) == expected
